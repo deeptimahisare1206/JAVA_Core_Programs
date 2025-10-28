c5acd2572d8b903c0f53d8fc452fdc3022f13803
Deepti Mahisare <deeptimahisare@gmail.com>
2025-10-28 18:37:31 +0530

    revision

--- Nested class/Membermain.java
+++ Nested class/Membermain.java
@@ -10,7 +10,7 @@ class MemberOuter{
 class Membermain{
 	public static void main(String args []){
 		MemberOuter Mo = new MemberOuter();
-		MemberOuter.MemberInner MOmi = Mo.new MemberInner();
-		MOmi.display();
+		MemberOuter.MemberInner MOmin = Mo.new MemberInner();
+		MOmin.display();
 	}
 }
