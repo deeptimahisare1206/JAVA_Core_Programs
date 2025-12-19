054a0e6c1046d684e51a8547cf5cb2f75db4ce91
Deepti Mahisare <115733808+deeptimahisare1206@users.noreply.github.com>
2025-12-19 22:12:48 +0530

    Update output messages in Parent and Derive classes

--- SuperMethod.java
+++ SuperMethod.java
@@ -1,14 +1,14 @@
 class Parent{
 	void show(){
-		System.out.println("Method of Super class....");
+		System.out.println("Method of Super(Parent) class....");
 	}
 }
 class Derive extends Parent{
 	String color = "Green";
 	void show(){
-	System.out.println("Methods of the Sub class....");
+	System.out.println("Methods of the Sub(Child) class....");
 	
-	super.show();g
+	super.show();
 	}
 }
 
