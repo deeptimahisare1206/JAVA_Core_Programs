3e28f5842e72c32ee559208262108303a8a4bbff
Deepti Mahisare <115733808+deeptimahisare1206@users.noreply.github.com>
2025-12-26 22:15:13 +0530

    Change name from 'Ramesh' to 'Ramshankar'

--- String/StringOdd.java
+++ String/StringOdd.java
@@ -1,6 +1,6 @@
 class StringOdd{
 	public static void main(String args []){
-		String name = ("Ramesh");
+		String name = ("Ramshankar");
 		int n = name.length();
 		for(int i = 0; i<n; i++){
 			if(i%2!=0){
