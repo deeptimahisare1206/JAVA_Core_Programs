443ac3fd9b33239232311c0d4f84e4edeb934e65
Deepti Mahisare <115733808+deeptimahisare1206@users.noreply.github.com>
2026-01-07 22:49:53 +0530

    Add 'w' to character array in String class

--- String/String.java
+++ String/String.java
@@ -1,6 +1,6 @@
 class String{
 	public static void main(String args[]){
-		char ch[]={'h','e','l','l','o'};
+		char ch[]={'h','e','l','l','o','w'};
 		String str= new String(ch);
 		System.out.println("String is: " +str);
 	}
