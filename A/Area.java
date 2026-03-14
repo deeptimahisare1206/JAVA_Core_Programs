1468c7878526e7bab4deeba76f55b6a899a7944c
Deepti Mahisare <115733808+deeptimahisare1206@users.noreply.github.com>
2026-03-14 22:47:41 +0530

    Correct output messages for radius and area

--- A/Area.java
+++ A/Area.java
@@ -4,11 +4,11 @@ public static void main(String args[]){
 int radius;
 float area;
 Scanner sc= new Scanner(System.in);
-System.out.println("Enter the radius: ");
+System.out.println("Enter the Radius: ");
 radius = sc.nextInt();
 
 area = 3.14f*radius*radius;
-System.out.println("the Area of circle is : "+area);
+System.out.println("The Area of circle is : "+area);
 
 }
 }
